3690625e60e13d493c01a94823355b80e8abbaca
AsokTamang <ashoktmg2058@gmail.com>
2025-12-12 00:22:05 -0500

    average method

--- query.sql
+++ query.sql
@@ -1,9 +1,2 @@
-/*
- Search for columns: brand, model, color, year, price, sold
- from the table cars
- where the color is a shade of red
- or the year is between 1960 and 1969
- and sold is false
- */
-SELECT sum(price) as total_earnings FROM cars
-WHERE sold is TRUE  --here we are using sum on price for determing the total earnings of the sold cars
+SELECT CEIL(AVG(price)) as average FROM cars --here ceil means math.ceil and AVG means average
+WHERE brand = 'Bentley'
