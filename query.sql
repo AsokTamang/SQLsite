b427649ed8a55cd8a9c6855327bab347b552b284
AsokTamang <ashoktmg2058@gmail.com>
2025-12-13 00:30:43 -0500

    update method

--- query.sql
+++ query.sql
@@ -1,6 +1,5 @@
-INSERT INTO cars
-(brand,model,year,price,color,condition,sold)
-VALUES('Chevrolet','Bel Air',1955,50000,'purple',5,false),
-('Porsche','944 Turbo',1986,48000,'white',4,false)
-
---so the syntax for inserting into the table using sql query is using INSERT INTO (column names) values()
+UPDATE cars
+SET condition = 1,
+    price = 10000
+WHERE brand = 'Porsche'
+    AND sold is FALSE
