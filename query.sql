d1a80147be5fa1ba1be7aaca1f3bd53192ffa1bc
AsokTamang <ashoktmg2058@gmail.com>
2025-12-13 00:23:39 -0500

    insert method

--- query.sql
+++ query.sql
@@ -1,4 +1,6 @@
-SELECT brand,model,year FROM cars
-WHERE sold is FALSE
-ORDER BY year
-LIMIT 5;
+INSERT INTO cars
+(brand,model,year,price,color,condition,sold)
+VALUES('Chevrolet','Bel Air',1955,50000,'purple',5,false),
+('Porsche','944 Turbo',1986,48000,'white',4,false)
+
+--so the syntax for inserting into the table using sql query is using INSERT INTO (column names) values()
