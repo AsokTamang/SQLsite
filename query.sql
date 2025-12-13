f0eb08cc8d4e6e1ecd7485d8f1a0404ad04da742
AsokTamang <ashoktmg2058@gmail.com>
2025-12-12 23:59:57 -0500

    complex combination

--- query.sql
+++ query.sql
@@ -1,5 +1,4 @@
-SELECT year,COUNT(year) AS car_count,MAX(price),MIN(price) FROM cars ---here count(year) counts the number of cars from this specific year
-WHERE sold IS TRUE
-GROUP BY year
-HAVING COUNT(sold=TRUE) > 1 --and the number of cars sold in the current specific year must be greater than 1
-ORDER BY car_count   --here order by car_count orders the result based on the count of cars in this specific year
+SELECT brand,model,year FROM cars
+WHERE sold is FALSE
+ORDER BY year
+LIMIT 5;
