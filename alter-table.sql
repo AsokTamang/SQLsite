4aa63bfb46a58c17c3b03ab4a6a59b5348a72aa8
AsokTamang <ashoktmg2058@gmail.com>
2025-12-15 10:38:53 -0500

    altering the foreign key

--- alter-table.sql
+++ alter-table.sql
@@ -1,6 +1,24 @@
 ALTER TABLE cars
 ADD COLUMN dealership_id INTEGER;
 
-UPDATE cars 
-SET dealership_id=1
+UPDATE cars
+SET dealership_id = 1
 WHERE dealership_id IS NULL;
+
+
+
+ALTER TABLE cars
+ALTER COLUMN dealership_id SET NOT NULL;
+
+ALTER TABLE cars
+ADD CONSTRAINT dealership_fk FOREIGN KEY(dealership_id) -- here we are declaring that the dealership_id in a table cars is the foreign key referencing to the id of the table dealerships
+REFERENCES dealerships(id);
+
+ALTER TABLE cars
+ALTER COLUMN brand SET NOT NULL,
+ALTER COLUMN model SET NOT NULL,
+ALTER COLUMN year SET NOT NULL, 
+ALTER COLUMN price SET NOT NULL,
+ALTER COLUMN color SET NOT NULL,
+ALTER COLUMN condition SET NOT NULL,
+ALTER COLUMN sold SET NOT NULL;
