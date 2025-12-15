8a12a355e83a0e1beee1144b1d9e0bb237329e3b
AsokTamang <ashoktmg2058@gmail.com>
2025-12-15 11:58:42 -0500

    UPDATE AND DELETE using foreign keys

---
+++ alter-constraint.sql
@@ -0,0 +1,42 @@
+ALTER TABLE staff
+ALTER column dealership_id DROP NOT NULL   --here we are deleting the not null constraint which was set on the column dealership_id of the table staff
+
+
+-- New salespeople
+INSERT INTO staff (name, role)
+ VALUES
+ ('Tony Turner', 'Salesperson'),
+ ('Axel Grimes', 'Salesperson'),
+ ('Elle Bowgrease', 'Salesperson');
+
+-- Insert new dealerships opening 2027
+INSERT INTO dealerships ( city, state, established )
+	VALUES
+	( 'Houston', 'TX', '2027-07-04' ),
+	( 'Phoenix', 'AZ', '2027-07-04' ),
+	( 'Dallas', 'TX', '2027-07-04' ),
+	( 'Austin', 'TX', '2027-07-04' ),
+	( 'Boston', 'MA', '2027-07-04');
+
+  /*
+	1. Alter table sold_cars
+		alter column seller
+		drop the not null constraint
+*/
+ALTER TABLE sold_cars
+ALTER COLUMN seller DROP NOT NULL;   --here we are deleting the not null constraint on the column seller of sold_cars
+/*
+	2. Update sold_cars setting the seller to null
+	     where the seller was Frankie
+	     hint: you can select his id from staff in query.js first
+*/
+UPDATE sold_cars SC SET seller=NULL 
+FROM staff S 
+WHERE SC.seller = S.id AND S.name='Frankie Fender';
+
+DELETE FROM staff
+WHERE name='Frankie Fender';
+
+/*
+	3. Delete Frankie Fender from the staff table
+*/
