e73845083b624befeaf9eae40858c58dc6cf77ac
AsokTamang <ashoktmg2058@gmail.com>
2025-12-16 11:43:04 -0500

    corrected comment

--- query.sql
+++ query.sql
@@ -2,5 +2,5 @@ SELECT name,role,D.city FROM sold_cars SC RIGHT JOIN staff S ON seller=S.id LEFT
 WHERE 
 SC.id is NULL AND S.role='Salesperson';
 
---here we are selecting the name and role of a staff who is a salesperson and who is the seller and who is also in the dealership but hasnot sold any cars till now, so
+--here we are selecting the name and role of a staff who is a salesperson and who might be or not be in the dealership but hasnot sold any cars till now, so
 --thats why in the where clause the sold_cars id here must be null
