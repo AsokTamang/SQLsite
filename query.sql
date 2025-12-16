0432e9a09e3cbed26583d15aa9b6dbf021966c58
AsokTamang <ashoktmg2058@gmail.com>
2025-12-16 13:30:22 -0500

    EXISTS method

--- query.sql
+++ query.sql
@@ -37,3 +37,8 @@ SELECT brand,model,city,price FROM cars JOIN dealerships D ON dealership_id=D.id
     SELECT sold_price from sold_cars
 )ORDER BY city;
 --here we are selecting the cars which are in dealerships and they are expensive than the cars that had been sold
+
+
+SELECT city,state,TO_CHAR(established,'YYYY-MM-DD') AS 'est' FROM dealerships WHERE NOT EXISTS (
+    SELECT 1 FROM cars WHERE dealership_id=dealerships.id 
+);--here we are selecting the cars whose deals have already been made but these cars are not in our stock
